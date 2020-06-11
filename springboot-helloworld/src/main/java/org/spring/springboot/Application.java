605bd57b6bba2f80458ce47b9fdb8fe238fb30db
luocaiwei <1072674645@qq.com>
2020-06-11 19:03:58 +0800

    第一次提交

--- springboot-helloworld/src/main/java/org/spring/springboot/Application.java
+++ springboot-helloworld/src/main/java/org/spring/springboot/Application.java
@@ -3,6 +3,9 @@ package org.spring.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Spring Boot 应用启动类
  *
@@ -15,6 +18,10 @@ public class Application {
     public static void main(String[] args) {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        SpringApplication.run(Application.class,args);
+        SpringApplication app = new SpringApplication(Application.class);
+        Map map = new HashMap();
+        map.put("server.port","9090");
+        app.setDefaultProperties(map);
+        app.run(args);
     }
 }
